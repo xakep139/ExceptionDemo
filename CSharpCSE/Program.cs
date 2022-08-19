3e688e077e0544d3d6e1512520f897614bdd9095
Nikita Balabaev <nbalabaev@microsoft.com>
2022-08-19 11:45:11 +0200

    more details for CSE demo

--- CSharpCSE/Program.cs
+++ CSharpCSE/Program.cs
@@ -14,23 +14,51 @@ namespace CSharpCSE
         {
             try
             {
-                throw new AccessViolation­Exception("Test exception");
-
-                #region Hint section
-                Marshal.StructureToPtr(123, new IntPtr(123), true);
-                #endregion
+                MyMethod();
             }
             catch (AccessViolationException ex)
             {
-                Console.WriteLine("AccessViolationException clause visited: {0}", ex.Message);
+                Console.WriteLine("AccessViolationException in Main(): {0}", ex.Message);
             }
             catch
             {
-                Console.WriteLine("Catch clause visited");
+                Console.WriteLine("Catch clause in Main()");
             }
             finally
             {
-                Console.WriteLine("Executing finally block");
+                Console.WriteLine("Executing finally block in Main()");
+            }
+        }
+
+        private class MyDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Disposing MyDisposable");
+            }
+        }
+
+        private static void MyMethod()
+        {
+            try
+            {
+                using (new MyDisposable())
+                {
+                    throw new AccessViolation­Exception("Test exception from MyMethod()");
+
+                    #region Hint section
+                    Marshal.StructureToPtr(123, new IntPtr(123), true);
+                    #endregion
+                }
+            }
+            catch (AccessViolation­Exception)
+            {
+                Console.WriteLine("AccessViolationException in MyMethod()");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block in MyMethod()");
             }
         }
     }
