f8e3b4023d62f85a227c66b17bdcc9b0d857be47
Nikita Balabaev <nbalabaev@microsoft.com>
2022-12-06 20:01:54 +0100

    Add OOM and SO examples

---
+++ OutOfMemoryAndStackOverflow/SoftOutOfMemory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+
+namespace SoftOutOfMemory;
+
+internal class Program
+{
+	static void Main(string[] _)
+	{
+		try
+		{
+			var list = new List<decimal[]>();
+			while (true)
+			{
+				list.Add(new decimal[100_500_000]);
+			}
+			//using var failPoint = new MemoryFailPoint(100_500);
+		}
+		catch (InsufficientMemoryException ex)
+		{
+			Console.WriteLine("Caught {0}", ex);
+		}
+		catch (OutOfMemoryException ex)
+		{
+			Console.WriteLine("We caught OutOfMemoryException: {0}", ex);
+		}
+
+		Console.WriteLine("Pgrogram terminated successfully...");
+	}
+}
+
---
+++ OutOfMemoryAndStackOverflow/StackOverflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StackOverflow;
+
+internal class Program
+{
+	static void Main(string[] _)
+	{
+		try
+		{
+			InfiniteRecursion(0);
+		}
+		catch (StackOverflowException)
+		{
+			Console.WriteLine("We caught StackOverflowException!");
+		}
+		catch (InsufficientExecutionStackException ex)
+		{
+			Console.WriteLine("Caught {0}", ex.Message);
+		}
+
+		Console.WriteLine("Pgrogram terminated successfully...");
+	}
+
+	static void InfiniteRecursion(int iteration)
+	{
+		//System.Runtime.CompilerServices.RuntimeHelpers.EnsureSufficientExecutionStack();
+		InfiniteRecursion(iteration + 1);
+		Console.WriteLine("Iteration {0} completed", iteration);
+	}
+}
